4056aa8b356119fba923f90f254cad7ab7129acc
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-10-09 14:54:09 +0530

    FIX: removed assets and access_config..js

--- src/config/access_config.js
+++
@@ -1,9 +0,0 @@
-const ACCESS_CONFIG = {
-  UsersCRMPage: ["ADMIN", "SALES"],
-  InternalDatasPage: ["ADMIN", "SALES"],
-  PlacementCRMPage: ["ADMIN", "PLACEMENT"],
-  TestimonialsPage: ["ADMIN", "MARKETING"],
-  SocialMediaPage: ["ADMIN", "MARKETING"],
-};
-
-export default ACCESS_CONFIG;
